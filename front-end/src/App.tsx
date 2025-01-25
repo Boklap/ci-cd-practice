db6fc7befda4888a58ee40333f0f30231627ea48
Boklap <setanmarco@gmail.com>
2025-01-25 12:24:07 +0700

    Update github action and front-end

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { axios } from axios
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
